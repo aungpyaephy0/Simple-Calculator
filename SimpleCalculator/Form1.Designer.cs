89918c2aad413d0f004c3b8363e05efc1d57f506
User <User@DESKTOP-KPO60EO>
2022-07-27 19:31:49 +0630

    Add project files.

---
+++ SimpleCalculator/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace SimpleCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.lblSec = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnDiv = new System.Windows.Forms.Button();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.txtSec = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirst.Location = new System.Drawing.Point(57, 38);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(152, 37);
+            this.lblFirst.TabIndex = 0;
+            this.lblFirst.Text = "First Number";
+            // 
+            // lblSec
+            // 
+            this.lblSec.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSec.Location = new System.Drawing.Point(57, 103);
+            this.lblSec.Name = "lblSec";
+            this.lblSec.Size = new System.Drawing.Size(152, 37);
+            this.lblSec.TabIndex = 1;
+            this.lblSec.Text = "Second Number";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(27, 179);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(50, 45);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "+";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMulti.Location = new System.Drawing.Point(176, 178);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(50, 45);
+            this.btnMulti.TabIndex = 4;
+            this.btnMulti.Text = "*";
+            this.btnMulti.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnMulti.UseVisualStyleBackColor = false;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(99, 178);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(50, 45);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = false;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnDiv
+            // 
+            this.btnDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDiv.Location = new System.Drawing.Point(246, 178);
+            this.btnDiv.Name = "btnDiv";
+            this.btnDiv.Size = new System.Drawing.Size(50, 45);
+            this.btnDiv.TabIndex = 6;
+            this.btnDiv.Text = "/";
+            this.btnDiv.UseVisualStyleBackColor = false;
+            this.btnDiv.Click += new System.EventHandler(this.btnDiv_Click);
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirst.Location = new System.Drawing.Point(229, 29);
+            this.txtFirst.Multiline = true;
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(192, 46);
+            this.txtFirst.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(321, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 45);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Close";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblDisplay.Font = new System.Drawing.Font("Arial", 14.25F);
+            this.lblDisplay.Location = new System.Drawing.Point(439, 29);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(244, 194);
+            this.lblDisplay.TabIndex = 10;
+            // 
+            // txtSec
+            // 
+            this.txtSec.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSec.Location = new System.Drawing.Point(229, 99);
+            this.txtSec.Multiline = true;
+            this.txtSec.Name = "txtSec";
+            this.txtSec.Size = new System.Drawing.Size(192, 46);
+            this.txtSec.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(699, 251);
+            this.Controls.Add(this.txtSec);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.btnDiv);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblSec);
+            this.Controls.Add(this.lblFirst);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.Label lblSec;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.TextBox txtFirst;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.TextBox txtSec;
+    }
+}
+
---
+++ SimpleCalculator/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace SimpleCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+  
+        }
+       
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(txtFirst.Text);
+            double secNum = double.Parse(txtSec.Text);
+            double total = firstNum + secNum;
+            lblDisplay.Text = "First Number: " + txtFirst.Text + "\n" + "\n"
+                              + "Second Number: " + txtSec.Text + "\n" + "\n"
+                              + "Result: " + total;
+        }
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(txtFirst.Text);
+            double secNum = double.Parse(txtSec.Text);
+            double total = firstNum - secNum;
+            lblDisplay.Text = "First Number: " + txtFirst.Text + "\n" + "\n"
+                              + "Second Number: " + txtSec.Text + "\n" + "\n"
+                              + "Result: " + total;
+        }
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(txtFirst.Text);
+            double secNum = double.Parse(txtSec.Text);
+            double total = firstNum * secNum;
+            lblDisplay.Text = "First Number: " + txtFirst.Text + "\n" + "\n"
+                              + "Second Number: " + txtSec.Text + "\n" + "\n"
+                              + "Result: " + total;
+        }
+        private void btnDiv_Click(object sender, EventArgs e)
+        {
+            double firstNum = double.Parse(txtFirst.Text);
+            double secNum = double.Parse(txtSec.Text);
+            double total = firstNum / secNum;
+            lblDisplay.Text = "First Number: " + txtFirst.Text + "\n"+"\n"
+                              + "Second Number: " + txtSec.Text + "\n" + "\n"
+                              + "Result: " + total;
+            if (firstNum/secNum == double.PositiveInfinity )
+            {
+                lblDisplay.Text = "Error";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtFirst.Text = "";
+            txtSec.Text = "";
+            lblDisplay.Text = "";
+        }
+       
+        
+    }
+}
